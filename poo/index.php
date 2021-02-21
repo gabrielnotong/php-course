2ebb471a1bf753463d3db44ed86e5b4a9f13136c
gnotong <g.notong@ubitransport.com>
2021-02-21 10:59:32 +0100

    init poo

---
+++ poo/index.php
@@ -0,0 +1,13 @@
+<?php
+$title = 'Home page';
+$active = 'index';
+require 'elements/header.php';
+?>
+
+<div id="container">
+    <section class="jumbotron">
+        <header>The index page</header>
+    </section>
+</div>
+
+<?php require 'elements/footer.php'; ?>
